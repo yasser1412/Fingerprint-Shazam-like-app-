500cac99abc79dbcfa3078a3dc7806711e99f511
khloud <61426989+Khloud-azeem@users.noreply.github.com>
2021-05-27 17:54:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,63 +1,75 @@
 from PyQt5 import QtWidgets, QtCore, uic, QtGui
 from PyQt5.QtWidgets import QMessageBox  
 from PyQt5.uic import loadUiType
-from os import path
-from scipy.io import wavfile
-import logging
 import sys
+import logging
 import os
+from os import path
+import numpy as np
+import librosa
+# import sounddevice as sd
 from pydub import AudioSegment
-from tempfile import mktemp
+import spectro_features
+
 
 logging.basicConfig(filename="logFile.log",format='%(asctime)s %(message)s',filemode='w')
 logger = logging.getLogger()
 logger.setLevel(20)
-MAIN_WINDOW,_=loadUiType(path.join(path.dirname(__file__),"task4.ui"))
 
-class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
+MAIN_WINDOW,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
+
+class Ui_MainWindow(QtWidgets.QMainWindow,MAIN_WINDOW):
     def __init__(self):
-        super(MainApp, self).__init__()
+        super(Ui_MainWindow, self).__init__()
         self.setupUi(self)
+
         self.slider.setEnabled(False)
         self.flag1 = False
         self.flag2 = False
         self.mixedAudio = None
-        self.labels = [self.label, self.label_2]
-        self.load1Btn.clicked.connect(lambda: self.loadsong(0))
-        self.load2Btn.clicked.connect(lambda: self.loadsong(1))
+        self.songs = [None,None]
+
+        self.connect_func()
+
+    def connect_func(self):
+        self.browseButton.clicked.connect(lambda: self.Browse(0))
+        self.browseButton_2.clicked.connect(lambda: self.Browse(1))
         self.slider.valueChanged.connect(lambda: self.sliderLabel.setText(str(self.slider.value())+"%"))
-        #self.searchBtn.clicked.connect(lambda: self.mixer())
-        self.audioDatas = [None, None]
-        self.audioRates = [None, None]
-            
-    def readAudio(self, audiopath):
-        logger.info("Reading first minute of the mp3")
-        #feh mo4kla hena
-        mp3_audio = AudioSegment.from_mp3(audiopath)[:60000] 
-        wname = mktemp('.wav')
-        mp3_audio.export(wname, format="wav", parameters=["-ac", "1"])
-        logger.info("Converted to wav")
-        samplingFreq, audioData = wavfile.read(wname)
-        logger.info("Read wav format and return the data")
-        return samplingFreq, audioData
-    
-    def loadsong(self , indx):
+        # self.searchBtn.clicked.connect(lambda: self.Mixer())
+
+    # def Load(self, file):
+        
+    #     return x , sr
+
+    def Browse(self,indx):
         self.statusbar.showMessage("Loading Audio File %s"%(indx+1))
-        audFile, audFormat = QtWidgets.QFileDialog.getOpenFileName(None, "Load Audio File %s"%(indx+1),filter="*.mp3")
-        logger.info("Audio File %s Loaded"%(indx+1))
-        if audFile == "":
-            logger.info("loading cancelled")
-            self.statusbar.showMessage("Loading cancelled")
-            pass
-        else:
-            logger.info("Loading data")
-            # sampRate, audioData = self.readAudio(audFile)
-            # self.audioDatas[indx] = audioData 
-            # self.audioRates[indx] = sampRate
-            self.labels[indx].setText(audFile.split('/')[-1])
-            self.statusbar.showMessage("Loading Done")
-            logger.info("Loading Done")
-            
+        file_name,_ = QtWidgets.QFileDialog.getOpenFileName(self, 'choose a song', os.getenv('HOME'),"")
+        wav_audio = "wav_audio.wav"
+        logger.info("Reading first minute of the mp3")        
+        mp3_audio = AudioSegment.from_mp3(file_name)[:60000] 
+        mp3_audio.export(wav_audio, format="wav")
+        logger.info("Converted to wav")
+        file_data , sr = librosa.load(wav_audio)
+        logger.info("Read wav format and return the data")
+        # logger.info("Audio File %s Loaded"%(indx+1))
+        # if file == "":
+        #     logger.info("loading cancelled")
+        #     self.statusbar.showMessage("Loading cancelled")
+        #     pass
+        # else:
+        logger.info("Loading data")
+        # file_data,sr = self.Load(file_name)
+        print(file_data, sr)
+        # self.labels[indx].setText(file.split('/')[-1])
+        self.statusbar.showMessage("Loading Done")
+        logger.info("Loading Done")
+        # sd.play(file_data, sr)
+        # plt.figure(figsize=(9, 5))
+        # librosa.display.waveplot(x, sr=sr)
+        # plt.show()
+        self.songs[indx] = spectro_features.Load_Song(file_name, file_data, sr)
+        print(self.songs)
+        
         if indx == 0:
             self.flag1 = True
         if indx == 1:
@@ -65,18 +77,76 @@ class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
         if self.flag1 == self.flag2 == True:
             self.slider.setEnabled(True)
     
-    def mixer(self):
-        logger.info("Start Mixing 2 songs")
-        w = self.slider.value()/100.0
-        self.mixedAudio = (w*self.audioDatas[0] + (1.0-w)*self.audioDatas[1])
-        logger.info("Mixing Done")
-        
-    
-def main():
+    # def Mixer(self):
+    #     logger.info("Start Mixing 2 songs")
+    #     w = self.slider.value()/100.0
+    #     self.mixedAudio = (w*self.audioDatas[0] + (1.0-w)*self.audioDatas[1])
+
+    # def Spectrogram(self, file, sr, indx):
+    #     #display Spectrogram
+    #     # X = librosa.stft(file)
+    #     # Xdb = librosa.amplitude_to_db(abs(X))
+    #     # plt.figure(figsize=(9, 5))
+    #     # librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') 
+    #     #If to pring log of frequencies  
+    #     #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
+    #     # plt.colorbar()
+    #     # plt.show()
+    #     spectro = librosa.feature.melspectrogram(file, sr=sr)
+    #     print('spectro', spectro)
+
+    # def Features(self, file, sr, indx):
+    #     #chroma_stft
+    #     chroma_stft = librosa.feature.chroma_stft(file, sr=sr)
+    #     self.song_features[0].append(self.Hash(chroma_stft))
+    #     # print(chroma_stft)
+
+    #     #zero_crossings
+    #     # zero_crossings = librosa.zero_crossings(file, pad=False)
+    #     # #print('sum of zero_crossings = ', sum(zero_crossings),zero_crossings_hash)
+    #     # self.song_features[0].append(self.Hash(zero_crossings))
+
+    #     #spectral centroid -- centre of mass -- weighted mean of the frequencies present in the sound
+    #     # import sklearn
+    #     spectral_centroids = librosa.feature.spectral_centroid(file, sr=sr)[0]
+    #     # spectral_centroids.shape
+    #     # Computing the time variable for visualization
+    #     # frames = range(len(spectral_centroids))
+    #     # t = librosa.frames_to_time(frames)
+    #     # Normalising the spectral centroid for visualisation
+    #     # def normalize(x, axis=0):
+    #     #     return sklearn.preprocessing.minmax_scale(x, axis=axis)
+    #     #Plotting the Spectral Centroid along the waveform
+    #     # librosa.display.waveplot(file, sr=sr, alpha=0.4)
+    #     # plt.plot(t, spectral_centroids, color='r')
+    #     # plt.show()
+    #     # print('spectral_centroids')
+    #     self.song_features[0].append(self.Hash(spectral_centroids))
+
+    #     #rolloff
+    #     # spectral_rolloff = librosa.feature.spectral_rolloff(file, sr=sr)[0]
+    #     # # librosa.display.waveplot(file, sr=sr, alpha=0.4)
+    #     # # plt.plot(t, spectral_rolloff, color='r')
+    #     # # print('rolloff')
+    #     # self.song_features[0].append(self.Hash(spectral_rolloff))
+
+
+    #     mfccs = librosa.feature.mfcc(file.astype('float64'), sr=sr)
+    #     # print(mfccs.shape)
+    #     #Displaying  the MFCCs:
+    #     # librosa.display.specshow(mfccs, sr=sr, x_axis='time')
+    #     # plt.show()
+    #     # print('mfccs')
+    #     self.song_features[0].append(self.Hash(mfccs))
+
+
+    # def Hash(self, feature) -> str:
+    #     data = Image.fromarray(feature)
+    #     return imagehash.phash(data, hash_size = 16).__str__()
+
+
+if __name__=='__main__':
     app = QtWidgets.QApplication(sys.argv)
-    window = MainApp()
+    window = Ui_MainWindow()
     window.show()
     sys.exit(app.exec_())
-
-if __name__ == "__main__":
-    main()
---
+++ spectro_features.py
@@ -0,0 +1,54 @@
+import numpy as np
+import librosa
+from PIL import Image
+import imagehash
+# from imagehash import hex_to_hash
+from scipy import signal
+
+
+def Load_Audio(file_name, file_data, sr):
+    song_name = file_name.split('/')[-1].split('.wav')[0]
+    song_data = file_data
+    song_datatype = file_data.dtype
+    song_dict = {
+        "name": song_name,
+        "data": song_data,
+        "sr": sr,
+        "dType": song_datatype,
+        "spectrogram_Hash": None,
+        "melspectrogram_Hash": None,
+        "mfcc_Hash": None,
+        "chroma_stft_Hash": None,
+    }
+    return song_dict
+
+
+def Features(file_data, sr, spectro):
+    #spectro
+    melspectro = librosa.feature.melspectrogram(file_data, sr=sr, S=spectro)
+    #chroma_stft
+    chroma_stft = librosa.feature.chroma_stft(file_data, sr=sr, S=spectro)
+    #spectral centroid -- centre of mass -- weighted mean of the frequencies present in the sound
+    # spectral_centroids = librosa.feature.spectral_centroid(file, sr=sr)[0]
+    mfccs = librosa.feature.mfcc(file_data.astype('float64'), sr=sr)
+
+    return [melspectro, mfccs, chroma_stft]
+
+
+def Hash(feature) -> str:
+    data = Image.fromarray(feature)
+    return imagehash.phash(data, hash_size = 16).__str__()
+
+
+def Load_Song(file_name,file_data,sr) -> dict:
+    #Loads audio file, create a spectrogram, extract some features and hash them.
+
+    song = Load_Audio(file_name, file_data, sr)
+    #colorMesh = sxx = spectrogram ndarray
+    f, t, colorMesh = signal.spectrogram(song['data'], fs=song['sr'], window='hann')
+    features = Features(song['data'], song['sr'], colorMesh)
+    song['spectrogram_Hash'] = Hash(colorMesh)
+    song['melspectrogram_Hash'] = Hash(features[0])
+    song['mfcc_Hash'] = Hash(features[1])
+    song['chroma_stft_Hash'] = Hash(features[2])
+    return song
