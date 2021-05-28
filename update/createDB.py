3c6ded64cf3b793555c982c4eaa0c927f1216948
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2021-05-28 03:35:54 +0200

    add

---
+++ update/createDB.py
@@ -0,0 +1,8 @@
+#create DataBase in JSON file in the format of:
+    # song_dict = {
+    #     "name": song_name,
+    #     "spectrogram_Hash": None,
+    #     "melspectrogram_Hash": None,
+    #     "mfcc_Hash": None,
+    #     "chroma_stft_Hash": None,
+    # } 
