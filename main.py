14e57f6f6e8e4ec7ae02ac4b4960ac9a94956df0
Yasser Nasser <48211831+yasser1412@users.noreply.github.com>
2021-05-29 12:45:56 +0200

    Done

--- main.py
+++ main.py
@@ -1,152 +1,148 @@
 from PyQt5 import QtWidgets, QtCore, uic, QtGui
-from PyQt5.QtWidgets import QMessageBox  
+from PyQt5.QtWidgets import QMessageBox
 from PyQt5.uic import loadUiType
-import sys
-import logging
-import os
 from os import path
-import numpy as np
-import librosa
-# import sounddevice as sd
-from pydub import AudioSegment
-import spectro_features
-
+import logging
+import sys
+import spectro_features 
+import load
+from createDB import read_json
 
 logging.basicConfig(filename="logFile.log",format='%(asctime)s %(message)s',filemode='w')
 logger = logging.getLogger()
 logger.setLevel(20)
+MAIN_WINDOW,_=loadUiType(path.join(path.dirname(__file__),"task4.ui"))
 
-MAIN_WINDOW,_=loadUiType(path.join(path.dirname(__file__),"main.ui"))
-
-class Ui_MainWindow(QtWidgets.QMainWindow,MAIN_WINDOW):
+class MainApp(QtWidgets.QMainWindow,MAIN_WINDOW):
     def __init__(self):
-        super(Ui_MainWindow, self).__init__()
+        super(MainApp, self).__init__()
         self.setupUi(self)
-
         self.slider.setEnabled(False)
         self.flag1 = False
         self.flag2 = False
-        self.mixedAudio = None
-        self.songs = [None,None]
-
-        self.connect_func()
-
-    def connect_func(self):
-        self.browseButton.clicked.connect(lambda: self.Browse(0))
-        self.browseButton_2.clicked.connect(lambda: self.Browse(1))
+        self.mixedAudioData = None
+        self.labels = [self.label, self.label_2]
+        self.load1Btn.clicked.connect(lambda: self.loadsong(0))
+        self.load2Btn.clicked.connect(lambda: self.loadsong(1))
         self.slider.valueChanged.connect(lambda: self.sliderLabel.setText(str(self.slider.value())+"%"))
-        # self.searchBtn.clicked.connect(lambda: self.Mixer())
-
-    # def Load(self, file):
+        self.searchBtn.clicked.connect(lambda: self.mixer())
+        self.audioDatas = [None, None]
+        self.audioRates = [None, None]
+        self.mixedSongDB = None
+        self.similarityResults = []
+    
+    def loadsong(self , indx):
+        self.statusbar.showMessage("Loading Audio File "+str(indx+1))
+        global audFile
+        audFile, audFormat = QtWidgets.QFileDialog.getOpenFileName(None, "Load Audio File "+str(indx+1),filter="*.mp3")
+        logger.info("Audio File %s Loaded"%(indx+1))
         
-    #     return x , sr
-
-    def Browse(self,indx):
-        self.statusbar.showMessage("Loading Audio File %s"%(indx+1))
-        file_name,_ = QtWidgets.QFileDialog.getOpenFileName(self, 'choose a song', os.getenv('HOME'),"")
-        wav_audio = "wav_audio.wav"
-        logger.info("Reading first minute of the mp3")        
-        mp3_audio = AudioSegment.from_mp3(file_name)[:60000] 
-        mp3_audio.export(wav_audio, format="wav")
-        logger.info("Converted to wav")
-        file_data , sr = librosa.load(wav_audio)
-        logger.info("Read wav format and return the data")
-        # logger.info("Audio File %s Loaded"%(indx+1))
-        # if file == "":
-        #     logger.info("loading cancelled")
-        #     self.statusbar.showMessage("Loading cancelled")
-        #     pass
-        # else:
-        logger.info("Loading data")
-        # file_data,sr = self.Load(file_name)
-        print(file_data, sr)
-        # self.labels[indx].setText(file.split('/')[-1])
-        self.statusbar.showMessage("Loading Done")
-        logger.info("Loading Done")
-        # sd.play(file_data, sr)
-        # plt.figure(figsize=(9, 5))
-        # librosa.display.waveplot(x, sr=sr)
-        # plt.show()
-        self.songs[indx] = spectro_features.Load_Song(file_name, file_data, sr)
-        print(self.songs)
-        
+        if audFile == "":
+            logger.info("loading cancelled")
+            self.statusbar.showMessage("Loading cancelled")
+            pass
+        else:
+            logger.info("Loading data")
+            global sampRate , audioData
+            try:
+                sampRate, audioData = load.readAudio(audFile)
+            except:
+                self.show_popup("File Not Supported", "Please Select Another File")
+                return
+            
+            self.audioDatas[indx] = audioData 
+            self.audioRates[indx] = sampRate
+            self.labels[indx].setText(audFile.split('/')[-1])
+            self.statusbar.showMessage("Loading Done")
+            logger.info("Loading Done")
+            
         if indx == 0:
             self.flag1 = True
+            
         if indx == 1:
             self.flag2 = True
+            
         if self.flag1 == self.flag2 == True:
             self.slider.setEnabled(True)
     
-    # def Mixer(self):
-    #     logger.info("Start Mixing 2 songs")
-    #     w = self.slider.value()/100.0
-    #     self.mixedAudio = (w*self.audioDatas[0] + (1.0-w)*self.audioDatas[1])
-
-    # def Spectrogram(self, file, sr, indx):
-    #     #display Spectrogram
-    #     # X = librosa.stft(file)
-    #     # Xdb = librosa.amplitude_to_db(abs(X))
-    #     # plt.figure(figsize=(9, 5))
-    #     # librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='hz') 
-    #     #If to pring log of frequencies  
-    #     #librosa.display.specshow(Xdb, sr=sr, x_axis='time', y_axis='log')
-    #     # plt.colorbar()
-    #     # plt.show()
-    #     spectro = librosa.feature.melspectrogram(file, sr=sr)
-    #     print('spectro', spectro)
-
-    # def Features(self, file, sr, indx):
-    #     #chroma_stft
-    #     chroma_stft = librosa.feature.chroma_stft(file, sr=sr)
-    #     self.song_features[0].append(self.Hash(chroma_stft))
-    #     # print(chroma_stft)
-
-    #     #zero_crossings
-    #     # zero_crossings = librosa.zero_crossings(file, pad=False)
-    #     # #print('sum of zero_crossings = ', sum(zero_crossings),zero_crossings_hash)
-    #     # self.song_features[0].append(self.Hash(zero_crossings))
-
-    #     #spectral centroid -- centre of mass -- weighted mean of the frequencies present in the sound
-    #     # import sklearn
-    #     spectral_centroids = librosa.feature.spectral_centroid(file, sr=sr)[0]
-    #     # spectral_centroids.shape
-    #     # Computing the time variable for visualization
-    #     # frames = range(len(spectral_centroids))
-    #     # t = librosa.frames_to_time(frames)
-    #     # Normalising the spectral centroid for visualisation
-    #     # def normalize(x, axis=0):
-    #     #     return sklearn.preprocessing.minmax_scale(x, axis=axis)
-    #     #Plotting the Spectral Centroid along the waveform
-    #     # librosa.display.waveplot(file, sr=sr, alpha=0.4)
-    #     # plt.plot(t, spectral_centroids, color='r')
-    #     # plt.show()
-    #     # print('spectral_centroids')
-    #     self.song_features[0].append(self.Hash(spectral_centroids))
-
-    #     #rolloff
-    #     # spectral_rolloff = librosa.feature.spectral_rolloff(file, sr=sr)[0]
-    #     # # librosa.display.waveplot(file, sr=sr, alpha=0.4)
-    #     # # plt.plot(t, spectral_rolloff, color='r')
-    #     # # print('rolloff')
-    #     # self.song_features[0].append(self.Hash(spectral_rolloff))
-
-
-    #     mfccs = librosa.feature.mfcc(file.astype('float64'), sr=sr)
-    #     # print(mfccs.shape)
-    #     #Displaying  the MFCCs:
-    #     # librosa.display.specshow(mfccs, sr=sr, x_axis='time')
-    #     # plt.show()
-    #     # print('mfccs')
-    #     self.song_features[0].append(self.Hash(mfccs))
-
-
-    # def Hash(self, feature) -> str:
-    #     data = Image.fromarray(feature)
-    #     return imagehash.phash(data, hash_size = 16).__str__()
-
-
-if __name__=='__main__':
+    def mixer(self):
+        w = self.slider.value()/100.0
+        
+        if self.flag1 == False and self.flag2 == False:
+            self.show_popup("No File Selected", "You Have To Select A File")
+            return
+        
+        if self.flag1 == True and self.flag2 == False:
+            self.mixedSongDB = spectro_features.Load_Song("Loaded Song", self.audioDatas[0], self.audioRates[0])
+            
+        if self.flag1 == False and self.flag2 == True:
+            self.mixedSongDB = spectro_features.Load_Song("Loaded Song", self.audioDatas[1], self.audioRates[1])
+            
+        if self.flag1 == self.flag2 == True: 
+            logger.info("Start Mixing 2 songs")
+            self.statusbar.showMessage("Mixing")    
+            self.mixedAudioData = (w*self.audioDatas[0] + (1.0-w)*self.audioDatas[1])
+            self.mixedSongDB = spectro_features.Load_Song("Loaded Song", self.mixedAudioData, self.audioRates[0])
+            logger.info("Mixing Done")
+            self.statusbar.showMessage("Mixing Done")
+        
+        self.check_similarity()
+    
+    def check_similarity(self):
+        logger.info("Searching and getting similarities")
+        self.statusbar.showMessage("Getting Similarities")
+        for songName, songHashes in read_json("db.json"):
+            
+            spectroDiff = spectro_features.get_hamming(songHashes["spectrogram_Hash"], self.mixedSongDB["Loaded Song"]["spectrogram_Hash"])
+            melSpectroDiff = spectro_features.get_hamming(songHashes["melspectrogram_Hash"], self.mixedSongDB["Loaded Song"]["melspectrogram_Hash"])
+            mffcDiff = spectro_features.get_hamming(songHashes["mfcc_Hash"], self.mixedSongDB["Loaded Song"]["mfcc_Hash"])
+            chromaDiff = spectro_features.get_hamming(songHashes["chroma_stft_Hash"], self.mixedSongDB["Loaded Song"]["chroma_stft_Hash"])
+            
+            output = (spectroDiff + melSpectroDiff + mffcDiff + chromaDiff)/4
+            self.similarityResults.append((songName , output*100))
+            
+        self.similarityResults.sort(key= lambda x: x[1], reverse=True)
+        logger.info("Searching and getting similarities Done")
+        self.statusbar.showMessage("Getting Similarities Done")   
+        
+        self.fill_table()
+    
+    def fill_table(self):
+        self.tableWidget.clear()
+        self.tableWidget.setRowCount(0)
+        self.tableWidget.setHorizontalHeaderLabels(["Found Matches", "Percentage"])
+        
+        logger.info("Showing Results")
+        self.statusbar.showMessage("Showing Results")   
+        for row in range(len(self.similarityResults)):
+            self.tableWidget.insertRow(row)
+            
+            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(self.similarityResults[row][0]))
+            self.tableWidget.setItem(row, 1, QtWidgets.QTableWidgetItem(str(round(self.similarityResults[row][1], 2))+"%"))
+            
+        for col in range(2):
+            self.tableWidget.horizontalHeader().setSectionResizeMode(col, QtWidgets.QHeaderView.Stretch)
+            self.tableWidget.horizontalHeaderItem(col).setBackground(QtGui.QColor(57, 65, 67))                        
+        self.similarityResults.clear()
+        
+        logger.info("Results Done")
+        self.statusbar.showMessage("Results Done")   
+    
+    def show_popup(self, message, information):
+        msg = QMessageBox()
+        msg.setWindowTitle("Message")
+        msg.setText(message)
+        msg.setIcon(QMessageBox.Warning)
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.setDefaultButton(QMessageBox.Ok)
+        msg.setInformativeText(information)
+        x = msg.exec_()    
+    
+def main():
     app = QtWidgets.QApplication(sys.argv)
-    window = Ui_MainWindow()
+    window = MainApp()
     window.show()
     sys.exit(app.exec_())
+
+if __name__ == "__main__":
+    main()
--- spectro_features.py
+++ spectro_features.py
@@ -1,54 +1,43 @@
-import numpy as np
 import librosa
 from PIL import Image
 import imagehash
-# from imagehash import hex_to_hash
+from imagehash import hex_to_hash
 from scipy import signal
 
-
-def Load_Audio(file_name, file_data, sr):
-    song_name = file_name.split('/')[-1].split('.wav')[0]
-    song_data = file_data
-    song_datatype = file_data.dtype
+def creat_dic(file_name):
     song_dict = {
-        "name": song_name,
-        "data": song_data,
-        "sr": sr,
-        "dType": song_datatype,
-        "spectrogram_Hash": None,
+        file_name: {"spectrogram_Hash": None,
         "melspectrogram_Hash": None,
         "mfcc_Hash": None,
-        "chroma_stft_Hash": None,
+        "chroma_stft_Hash": None}
     }
     return song_dict
-
 
 def Features(file_data, sr, spectro):
     #spectro
     melspectro = librosa.feature.melspectrogram(file_data, sr=sr, S=spectro)
     #chroma_stft
     chroma_stft = librosa.feature.chroma_stft(file_data, sr=sr, S=spectro)
-    #spectral centroid -- centre of mass -- weighted mean of the frequencies present in the sound
-    # spectral_centroids = librosa.feature.spectral_centroid(file, sr=sr)[0]
+    #mfccs
     mfccs = librosa.feature.mfcc(file_data.astype('float64'), sr=sr)
-
     return [melspectro, mfccs, chroma_stft]
 
-
-def Hash(feature) -> str:
+def Hash(feature):
     data = Image.fromarray(feature)
     return imagehash.phash(data, hash_size = 16).__str__()
 
-
-def Load_Song(file_name,file_data,sr) -> dict:
+def Load_Song(file_name,file_data,sr):
     #Loads audio file, create a spectrogram, extract some features and hash them.
-
-    song = Load_Audio(file_name, file_data, sr)
+    song = creat_dic(file_name)
     #colorMesh = sxx = spectrogram ndarray
-    f, t, colorMesh = signal.spectrogram(song['data'], fs=song['sr'], window='hann')
-    features = Features(song['data'], song['sr'], colorMesh)
-    song['spectrogram_Hash'] = Hash(colorMesh)
-    song['melspectrogram_Hash'] = Hash(features[0])
-    song['mfcc_Hash'] = Hash(features[1])
-    song['chroma_stft_Hash'] = Hash(features[2])
+    f, t, colorMesh = signal.spectrogram(file_data, fs=sr, window='hann')
+    features = Features(file_data, sr, colorMesh)
+    song[file_name]["spectrogram_Hash"] = Hash(colorMesh)
+    song[file_name]['melspectrogram_Hash'] = Hash(features[0])
+    song[file_name]['mfcc_Hash'] = Hash(features[1])
+    song[file_name]['chroma_stft_Hash'] = Hash(features[2])
     return song
+
+def get_hamming(hash1, hash2):
+    similarity = 1 - ( hex_to_hash(hash1) - hex_to_hash(hash2) )/256.0
+    return similarity
