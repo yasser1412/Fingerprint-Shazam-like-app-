83ed3acb4ff1ba60d0b7403f783b894a198849ee
khloud <61426989+Khloud-azeem@users.noreply.github.com>
2021-05-31 19:22:42 +0200

    Add files via upload

--- createDB.py
+++ createDB.py
@@ -7,20 +7,19 @@ def updateDB():
     jsonData = {}
     for file in os.scandir(r"songs"):
         if (file.path.endswith(".mp3")):
-            try:
-                sampRate, audioData = load.readAudio(file)
-                songName = file.path.split('\\')[-1]
-                data = spectro_features.Load_Song(songName, audioData, sampRate)
-                jsonData.update(data)
-            except:
-                pass
+            sampRate, audioData = load.readAudio(file)
+            songName = file.path.split('\\')[-1]
+            data = spectro_features.Load_Song(songName, audioData, sampRate)
+            jsonData.update(data)
     with open("db.json", "a") as outfile:
         json.dump(jsonData, outfile, indent=4)
 
-def read_json(path):
-    with open(path) as jsonFile:
+
+def readJson(file):
+    with open(file) as jsonFile:
         data = json.load(jsonFile)
     for song in data:
         yield song, data[song]
 
-# updateDB()
+if __name__ == '__main__':
+    updateDB()
